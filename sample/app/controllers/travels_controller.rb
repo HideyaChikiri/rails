eb3f19bf7327b32a7431259861484ffe9cf07b76
bun1006 <hideya2013.uma@gmail.com>
2016-06-03 14:17:24 +0000

    finish 3.7

--- sample/app/controllers/travels_controller.rb
+++ sample/app/controllers/travels_controller.rb
@@ -1,7 +1,8 @@
 class TravelsController < ApplicationController
   def show
     # render :text => "upper = #{params[:upper]}, lower = #{params[:lower]}"
-    render :text => "旅行先 = #{params[:dest]}, 人数 = #{params[:num]}"
-    
+    # render :text => "旅行先 = #{params[:dest]}, 人数 = #{params[:num]}"
+    travel = params[:travel]
+    render :text => "行きたい場所 = #{travel[0]}, #{travel[1]}, #{travel[2]}"
   end
 end
